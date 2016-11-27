0d5d7255563f4ed8bb25a44a9efc89acae819a2c
Yaoguais <newtopstdio@163.com>
2016-11-28 00:57:38 +0800

    quick sort compare & benchmark

---
+++ algorithm/repeat0/ExchageSelect/normal_quick_sort.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+void quick_sort(int arr[], int left, int right);
+
+int main() {
+	const int len = 1000;
+    int i, l,  arr[len];
+	for (i = 0; i< len; i++) {
+		arr[i] = i;
+	}
+	for (i = 0; i< 100; i++) {
+    	quick_sort(arr, 0, sizeof(arr) / sizeof(int) - 1);
+	}
+
+    return 0;
+}
+
+void swap(int * a, int * b) {
+	int t = *a;
+	*a = *b;
+	*b = t;
+}
+
+int partition(int arr[], int left, int right) {
+	int i, p;
+	p = left;	
+	for (i = left + 1; i <= right; i++) {
+		if (arr[i] <= arr[left]) {
+			p++;
+			if (p != i) {
+				swap(&arr[p], &arr[i]);
+			}
+		}	
+	}
+	swap(&arr[left], &arr[p]);
+
+	return p;
+}
+
+void quick_sort(int arr[], int left, int right) {
+	int p;
+	if (left < right) {
+		p = partition(arr, left, right);
+		quick_sort(arr, left, p - 1);
+		quick_sort(arr, p + 1, right);
+	}	
+}
+
