4028341d4ce04e89f1ed2f6d62f6d27f4c03fab3
Yaoguais <newtopstdio@163.com>
2017-01-07 18:29:42 +0800

    close mysql slave

--- config/docker_devel/start.sh
+++ config/docker_devel/start.sh
@@ -3,6 +3,6 @@
 # start develop cluster
 
 sudo docker-compose up -d
-sleep 2
-echo "start slave;" | mysql --protocol tcp -h127.0.0.1 -P3307 -uroot -pyaoguais_2014
-echo
+# sleep 2
+# echo "start slave;" | mysql --protocol tcp -h127.0.0.1 -P3307 -uroot -pyaoguais_2014
+# echo
