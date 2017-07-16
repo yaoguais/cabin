0c0924db388a3f575787fbf3f86ca2bc30b97491
Yaoguais <newtopstdio@163.com>
2017-07-16 14:10:19 +0800

    fix orders

--- tidb/trans_test/mysqlnd.trace.2.separate.php
+++ tidb/trans_test/mysqlnd.trace.2.separate.php
@@ -2,12 +2,12 @@
 $file = __DIR__ . '/mysqlnd.trace.2';
 $dir = __DIR__ . '/mysqlnd.trace.2.separate';
 mkdir($dir);
-$contents = file_get_contents($file);
-$rows = explode("\n", $contents);
-foreach ($rows as $row) {
-    $items = explode(':', $row, 2);
+$fp = fopen($file, "r");
+while($line = fgets($fp)) {
+    $items = explode(':', $line, 2);
     if (strlen($items[0]) > 0) {
         $outFile = $dir . '/' . $items[0] . '.log';
-        file_put_contents($outFile, $row . "\n", FILE_APPEND);
+        file_put_contents($outFile, $line, FILE_APPEND);
     }
 }
+fclose($fp);
